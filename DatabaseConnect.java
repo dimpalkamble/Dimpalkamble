2f129e6193cfaa70030b908daebcd004f263ab2c
dimpalkamble <137601181+dimpalkamble@users.noreply.github.com>
2023-06-24 00:39:32 -0700

    Add files via upload

---
+++ DatabaseConnect.java
@@ -0,0 +1,34 @@
+package com.tka.jdbc;
+
+import java.sql.*;
+public class DatabaseConnect {
+	
+	
+	static void connect()
+	{
+		try
+		{
+	Class.forName("com.mysql.jdbc.Driver");
+	
+	Connection con = DriverManager.getConnection(
+	"jdbc:mysql://localhost:3306/aprojt1", "root", "root");
+	System.out.println("Connected to the database");
+		}
+		catch(ClassNotFoundException e)
+		{
+			System.out.println(e);
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		connect();
+		
+
+	}
+
+}
